b43dc832416aa3a63851f63e3015ac1857590c31
SRB <souhardaroybarman05@gmail.com>
2025-06-11 22:19:29 +0530

    feat: Refactor WorkspaceHeader layout for improved structure and styling

--- app/components/ui/workspace/WorkspaceHeader.tsx
+++ app/components/ui/workspace/WorkspaceHeader.tsx
@@ -19,37 +19,46 @@ interface WorkspaceHeaderProps{
 
 const WorkspaceHeader = ({workspace,members,onCreateProject,onInviteMember}:WorkspaceHeaderProps) => {
   
-  
   return (
     <div className='space-y-3'>
       <div className='space-y-1'>
-        <div className='flex flex-col-reverse md:flex-row md:justify-between md:items-center gap-3'>
-          <div className='flex md:items-center gap-3'>
+        <div className='flex flex-row md:mt-0 mt-5 mb-5 md:flex-row md:justify-between md:items-center gap-3'>
+          <div className='flex items-center gap-3 '>
             {
               workspace.color && (
-                <WorkspaceAvatar color={workspace.color} name={workspace.name}/>
+                <WorkspaceAvatar  color={workspace.color} name={workspace.name}/>
               )
             }
-            <h2 className='text-xl md:text-2xl font-semibold'>
-              {workspace.name}
-            </h2>
+            <div className='flex flex-col md:block'>
+              <h2 className='text-xl md:text-2xl font-semibold'>
+                {workspace.name}
+              </h2>
+             
+            </div>
           </div>
-          <div className='flex items-center gap-3 justify-end md:justify-start mb-4 md:mb-0 mt-3 '>
-            <Button variant="glassMorph" onClick={onInviteMember}>
-              <UserPlus  className='size-4 mr-2'/> Invite Member
+          <div className='flex ml-10 md:ml-0 gap-3 md:gap-4 flex-wrap'>
+            <Button 
+              variant="glassMorph" 
+              onClick={onInviteMember}
+              className='text-sm md:text-base'
+            >
+              <UserPlus className='size-4 mr-2'/> Invite Member
             </Button>
-            <Button variant='neomorphic' onClick={onCreateProject}><PlusCircle className='size-4 mr-2'/> Create Project</Button>
+            <Button 
+              variant='neomorphic' 
+              onClick={onCreateProject}
+              className='text-sm md:text-base'
+            >
+              <PlusCircle className='size-4 mr-2'/> Create Project
+            </Button>
           </div>
         </div>
-        {
-          workspace.description && (
-            <p className='text-sm md:text-base dark:text-muted-foreground'>
-              <span className='text-primary'>Description : </span>{workspace.description}
-            </p>
-          )
-        }
-      </div>
-      {
+         {workspace.description && (
+                <p className='text-sm md:text-base dark:text-muted-foreground md:mt-0 mt-1'>
+                  <span className='text-primary'>Description : </span>{workspace.description}
+                </p>
+              )}
+               {
         members.length > 0 && (
           <div className='flex items-center gap-2'>
             <span className='text-sm dark:text-primary'>
@@ -59,7 +68,6 @@ const WorkspaceHeader = ({workspace,members,onCreateProject,onInviteMember}:Work
               {
                 members.map((member) => (
                   <Avatar key={member._id} className='relative h-8 w-8 rounded-full border-2 border-background overflow-hidden'
-                  
                   title={member.user.name}>
                     <AvatarImage 
                     src={member.user.profilePicture} 
@@ -77,6 +85,7 @@ const WorkspaceHeader = ({workspace,members,onCreateProject,onInviteMember}:Work
           </div>
         )
       }
+      </div>
     </div>
   )
 }
