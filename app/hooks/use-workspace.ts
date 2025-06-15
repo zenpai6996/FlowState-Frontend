002ae52abd10d9dad4be3504869d379e7e237987
SRB <souhardaroybarman05@gmail.com>
2025-06-15 17:50:08 +0530

    feat: enhance workspace functionality; add stats and task trend types, update dashboard to utilize workspace stats

--- app/hooks/use-workspace.ts
+++ app/hooks/use-workspace.ts
@@ -1,23 +1,30 @@
-import { useMutation, useQuery } from "@tanstack/react-query"
-import type { WorkSpaceForm } from "~/components/ui/workspace/CreateWorkspace"
-import { fetchData, postData } from "~/lib/fetch-utils"
+import { useMutation, useQuery } from "@tanstack/react-query";
+import type { WorkSpaceForm } from "~/components/ui/workspace/CreateWorkspace";
+import { fetchData, postData } from "~/lib/fetch-utils";
 
 export const useCreateWorkspace = () => {
-  return useMutation({
-    mutationFn: async (data : WorkSpaceForm) => postData("/workspaces",data)
-  })
-}
+	return useMutation({
+		mutationFn: async (data: WorkSpaceForm) => postData("/workspaces", data),
+	});
+};
 
 export const useGetWorkspacesQuery = () => {
-  return useQuery({
-    queryKey:["workspaces"],
-    queryFn: async () => fetchData("/workspaces"),
-  });
-}
+	return useQuery({
+		queryKey: ["workspaces"],
+		queryFn: async () => fetchData("/workspaces"),
+	});
+};
 
-export const useGetWorkspaceById = (workspaceId:string) => {
-  return useQuery({
-    queryKey:["workspace",workspaceId],
-    queryFn: async () => fetchData(`/workspaces/${workspaceId}/projects`),
-  })
-}
+export const useGetWorkspaceById = (workspaceId: string) => {
+	return useQuery({
+		queryKey: ["workspace", workspaceId],
+		queryFn: async () => fetchData(`/workspaces/${workspaceId}/projects`),
+	});
+};
+
+export const useGetWorkspaceStats = (workspaceId: string) => {
+	return useQuery({
+		queryKey: ["workspace", workspaceId, "stats"],
+		queryFn: async () => fetchData(`/workspaces/${workspaceId}/stats`),
+	});
+};
--- app/routes/dashboard/index.tsx
+++ app/routes/dashboard/index.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { useSearchParams } from "react-router";
+import { useGetWorkspaceStats } from "~/hooks/use-workspace";
+import type { StatsProps, TaskTrendProps } from "~/types";
 
 const index = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+	const [searchParams] = useSearchParams();
+	const workspaceId = searchParams.get("workspaceId");
+	const { data, isPending } = useGetWorkspaceStats(workspaceId!) as {
+		data: {
+			stats: StatsProps;
+			taskTrendData: TaskTrendProps[];
+		};
+		isPending: boolean;
+	};
+	return <div>Dashboard</div>;
+};
 
-export default index
+export default index;
